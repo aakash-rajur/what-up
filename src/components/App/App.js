9b1077475c13e88b30353d077832b169ff733219
Aakash Rajur <aakashrajur16@gmail.com>
2018-08-07 23:41:50 +0530

    css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -92,13 +92,13 @@ class App extends Component {
                     {this.renderTaskList}
                 </Query>
 
-                <footer className="documentation">
+                <div className="documentation">
                     <em>double-click to edit tasks</em><br/>
                     Created by <a href="https://github.com/aakashRajur"
                                   className="link">aakashRajur</a><br/>
                     Inspired by <a href="http://todomvc.com/examples/react/" className="link">React •
                     TodoMVC</a><br/>
-                </footer>
+                </div>
             </Fragment>
         );
     }
