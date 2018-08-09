606b306fb89979e72a3dc11ccdd2234880b75f60
Aakash Rajur <aakashrajur16@gmail.com>
2018-08-09 21:39:46 +0530

    task component test cases complete

--- src/components/Task/Task.test.js
+++ src/components/Task/Task.test.js
@@ -29,6 +29,18 @@ describe('Task component non editable description', () => {
 		);
 	});
 	
+	it('state integrity', () => [{
+			prop: 'description',
+			value: tasks[0].description
+		}, {
+			prop: 'editable',
+			value: false
+		}].forEach(({prop, value}) =>
+			expect(component.state(prop))
+				.toBe(value)
+		)
+	);
+	
 	[{
 		test: 'status',
 		selector: 'div.icon.status',
@@ -61,48 +73,61 @@ describe('Task component editable description', () => {
 			updateTask,
 			cancelTask,
 			editTask
-		},
-		component = shallow(<Task {...props}/>);
+		}, component = mount(<Task {...props}/>);
 	
-	it('should render', () => {
+	it('should render', () =>
 		expect(component.find('div.app-width.task').exists())
-			.toBe(true);
-	});
-	
-	component.setState({editable: true});
-	
-	component.update();
-	
-	it('dom integrity', () => {
-		[
-			'div.icon.status',
-			'img.icon.cancel'
-		].forEach(child =>
-			expect(component.find(child).exists())
-				.toBe(true)
-		);
-	});
-	
-	/*
-	[{
-		test: 'status',
-		selector: 'div.icon.status',
-		callback: 'updateTask'
-	}, {
-		test: 'cancel',
-		selector: 'img.icon.cancel',
-		callback: 'cancelTask'
-	}].forEach(({test, selector, callback}) =>
-		it(`should invoke ${callback} on ${test} click`, () => {
-			component.find(selector)
-				.simulate('click');
-			let func = props[callback];
-			expect(func.called).toBe(true);
-			expect(func.callCount).toBe(1);
-		})
+			.toBe(true)
 	);
+	
+	component.find('div.description')
+		.simulate('doubleClick');
+	
+	it('state integrity after', () => [{
+			prop: 'description',
+			value: tasks[0].description
+		}, {
+			prop: 'editable',
+			value: true
+		}].forEach(({prop, value}) =>
+			expect(component.state(prop))
+				.toBe(value)
+		)
+	);
+	
+	it('dom integrity', () => [
+		'div.icon.status',
+		'input.editable',
+		'img.icon.cancel'
+	].forEach(child =>
+		expect(component.find(child).exists())
+			.toBe(true)
+	));
+	
+	it('should invoke relevant callback', () => {
+		[{
+			test: 'status',
+			selector: 'div.icon.status',
+			callback: 'updateTask'
+		}, {
+			test: 'cancel',
+			selector: 'img.icon.cancel',
+			callback: 'cancelTask'
+		}].forEach(({test, selector, callback}) => {
+				component.find(selector)
+					.simulate('click');
+				let func = props[callback];
+				expect(func.called).toBe(true);
+				expect(func.callCount).toBe(1);
+			}
+		);
+		component.find('input.editable')
+			.simulate('keyup', {keyCode: 13, target: {value: props.value}});
+		expect(props.editTask.called).toBe(true);
+		expect(props.editTask.callCount).toBe(1);
+	});
 	
 	it('match snapshot', () => {
 		expect(component).toMatchSnapshot();
-	});*/
+	});
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,30 @@
 import {configure, mount, render, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
+import {JSDOM} from 'jsdom';
 import fetch from 'node-fetch';
-import {stub, spy} from 'sinon';
+import {spy, stub} from 'sinon';
 import WebSocket from 'ws';
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+	const props = Object.getOwnPropertyNames(src)
+		.filter(prop => typeof target[prop] === 'undefined')
+		.reduce((result, prop) => ({
+			...result,
+			[prop]: Object.getOwnPropertyDescriptor(src, prop),
+		}), {});
+	Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+	userAgent: 'node.js',
+};
+copyProps(window, global);
 
 configure({adapter: new Adapter()});
 
@@ -12,5 +33,6 @@ global.render = render;
 global.mount = mount;
 global.stub = stub;
 global.spy = spy;
+
 global.WebSocket = WebSocket;
 global.fetch = fetch;
