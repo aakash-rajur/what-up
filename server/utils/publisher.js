fd63eee6817c5eb8e2b5e19f12c746c0e5d492b9
Aakash Rajur <aakashrajur16@gmail.com>
2018-09-23 16:41:19 +0530

    context binding and deferred publishing

--- server/utils/publisher.js
+++ server/utils/publisher.js
@@ -5,10 +5,15 @@ let instance = null;
 
 class Publisher {
 	constructor() {
+		this.notify = this.notify.bind(this);
+		this.notifyDeferred = this.notifyDeferred.bind(this);
 		this.pubsub = new PubSub();
+		this.asyncIterator = this.pubsub.asyncIterator.bind(this.pubsub);
 	}
 	
-	notify(event, data = {}) {
+	async notify(event, data = {}) {
+		if (data instanceof Function)
+			data = await data();
 		this.pubsub.publish(event, {
 			[event]: {
 				...data,
@@ -17,8 +22,8 @@ class Publisher {
 		});
 	}
 	
-	asyncIterator(...args) {
-		return this.pubsub.asyncIterator(...args);
+	notifyDeferred(event, data = {}) {
+		setImmediate(this.notify, event, data);
 	}
 }
 
